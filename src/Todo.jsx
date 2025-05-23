d1f82a3f526bc0004bcf5ab926076b9407f58953
sakthi328 <sakthisankar328@gmail.com>
2025-05-23 07:51:12 +0530

    Todo with edit and delete

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -1,32 +1,35 @@
 import React, { useState } from "react";
+import { TodoList } from "./TodoList";
 
 
 function Todo(){
-    const [Number,setNumber] = useState(2);
     const [todoList, setTodoList] = useState([]);
-    const [task, setTask] = useState('');
+    const [task, setTask] = useState("");
+    
+    const handleAddTask = () =>{
+     if(task == "") {
+        alert("Task should not be empty")
+     }else{
+        setTodoList((prev) => [...prev, task]);
+        setTask("");
 
+     }  
+    } 
 
-    const handleAddTask = ()=>{
-        if(task ===''){
-            alert("Task should not be empty");
-        }else{
-            setTodoList(prev=>([...prev,task]));
-            setTask('');
-        }
-        
-    }
+ return(
+    <div>
+        <h1>Todo app</h1>
+        <input value={task} name="todo-list" style={ {height :"40px", width:"520px"}} onChange={(e) => setTask(e.target.value)} />
+        <button onClick={handleAddTask} style={{ marginLeft: "5px", backgroundColor:"green", color:"white"}}>Add Task</button>
+        <div style={{ margin: "10px 0" }}>
+            {todoList.map((item,index) => (
+          <TodoList key={index} index={index} item={item} setTodoList={setTodoList} />
+        ))}
 
-    return(<div>
-        <h1>
-            Daily Todo
-        </h1>
-        <input value={task} name="todo-list" style={{"height":"40px"}} onChange={(e)=>setTask(e.target.value)} />
-        <button onClick={handleAddTask} style={{"marginLeft":"5px"}} >
-            Add Task
-        </button>
-        {todoList.map((item)=><p>{item}</p>)}
-    </div>)
+        </div>
+    </div>
+ )   
+
 };
 
 
---
+++ src/TodoList.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+export const TodoList = (props) => { 
+    const { item, index, setTodoList } = props;
+    const [isEdit, setIsEdit] = useState(false);
+    const container_style = {
+        minWidth: "520px",
+        padding: "5px 10px",
+        borderBottom: "1px solid #ccc",
+        textAlign: "start",
+        display: "flex",
+        justifyContent: "space-between",
+    };
+
+    const handleEditChange = (e) => {
+        const { value } = e.target;
+        setTodoList(prev => (prev.map((list, Index) => index === Index ? value : list)));
+    };
+    const handleDelete = () =>{
+        setTodoList(prev=>(prev.filter((list,Index)=>Index !==index)))
+    }
+    return(
+        <div style={container_style}>
+            {isEdit ? <input type="text" onChange={handleEditChange} value={item} /> : <p>{item}</p>}
+            <div>
+                <button style={{backgroundColor:"orange", color:"white"}} onClick={() => setIsEdit(!isEdit)}>Edit</button>{" "}
+                <button style={{backgroundColor:"red", color:"white"}} onClick={handleDelete}>Delete</button>
+            </div>
+        </div>
+    );
+};
